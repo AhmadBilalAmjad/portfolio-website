3954beb1e99235343aa6b104cf8338f591dde423
AhmadBilalAmjad <ahmad.bilal.amjad@outlook.com>
2024-02-13 19:16:22 +0500

    next js version updated

--- next.config.js
+++ next.config.js
@@ -7,7 +7,10 @@ const nextConfig = {
         hostname: "avatars.githubusercontent.com",
       }
     ],
-  }
+  },
+  experimental: {
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
