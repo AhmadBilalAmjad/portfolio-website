697c92231611a18d35410244f14a771138ac156c
AhmadBilalAmjad <ahmad.bilal.amjad@outlook.com>
2024-02-13 19:46:22 +0500

    email configuration added

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
 import SectionHeading from "./section-heading";
 import {
   VerticalTimeline,
@@ -11,17 +10,6 @@ import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 import { useTheme } from "@/context/theme-context";
-
-{/* <div className="mt-2 leading-relaxed flex flex-col sm:items-start sm:text-left text-center items-center space-y-1">
-  <div>
-    <span className="bg-gray-100 text-gray-500 w-4 h-4 mr-2 border border-gray-500 rounded-full inline-flex items-center justify-center">
-      <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" className="w-3 h-3" viewBox="0 0 24 24">
-        <path d="M20 6L9 17l-5-5"></path>
-      </svg>
-    </span>
-    <span className="text-gray-700 dark:text-white/70">des</span>
-  </div>
-</div> */}
 
 export default function Experience() {
   const { ref } = useSectionInView("Experience");
